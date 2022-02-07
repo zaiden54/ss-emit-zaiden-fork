80bcf87840a4910357286427efcb70f32b120119
zaiden54 <you@example.com>
2022-02-08 00:58:29 +0300

    added footer

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,12 +1,14 @@
 import { Navbar } from '../components/Navbar'
 import React from 'react'
 import { Slider } from '../components/Slider'
+import { Footer } from '../components/Footer'
 
 export const MainPage = () => {
     return(
         <div>
             <Navbar />
             <Slider />
+            <Footer />
         </div>
     )
 }
