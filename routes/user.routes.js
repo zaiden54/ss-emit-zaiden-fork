2228eb508350dee8eebb366fd80fdf605c8f56c4
Зуи Линь <NGUZUI@lan.rarus.ru>
2022-03-18 16:03:42 +0300

    Merge with zaiden

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,11 +8,6 @@ router.get( '/info', async (req, res) => {
     try {
         const { userId } = req.query
 
-<<<<<<< HEAD
-        console.log(userId)
-
-=======
->>>>>>> c4fc8f24a5596091bdb8d640b54fcb13db781f95
         const user = await Users.findOne({ _id: userId} )
 
         res.send(user)
